ae4d3ef461b77a4936163012d3465d2c27de6b1e
Benedict Smith <benedict.smith@ukaea.uk>
2026-08-05 13:23:10 +0100

    Added dummy files created by splitMeshRegions; got basic working snappyHexMesh (note that symmetryPlane patches still don't work)

--- base_case/runMesh.sh
+++ base_case/runMesh.sh
@@ -1,3 +1,5 @@
-rm -rf constant/polyMesh
-blockMesh 2>&1 | tee log.blockMesh
+set -eou pipefail
+rm -rf constant/polyMesh constant/*/polyMesh
+blockMesh -dict system/blockMeshDict 2>&1 | tee log.blockMesh
 snappyHexMesh -overwrite 2>&1 | tee log.snappyHexMesh
+splitMeshRegions -cellZones -overwrite 2>&1 | tee log.splitMeshRegions

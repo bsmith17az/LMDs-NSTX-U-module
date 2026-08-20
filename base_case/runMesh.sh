c8bea0066c1847c3040dbc7a18feb36e20732e3e
Benedict Smith <benedict.smith@ukaea.uk>
2026-08-20 09:09:36 +0100

    Place field files in new directory for copying across

--- base_case/runMesh.sh
+++ base_case/runMesh.sh
@@ -1,5 +1,6 @@
 set -eou pipefail
 rm -rf constant/polyMesh constant/*/polyMesh dynamicCode
 blockMesh -dict system/blockMeshDict 2>&1 | tee log.blockMesh
+decomposePar 
 snappyHexMesh -overwrite 2>&1 | tee log.snappyHexMesh
 splitMeshRegions -cellZones -overwrite 2>&1 | tee log.splitMeshRegions

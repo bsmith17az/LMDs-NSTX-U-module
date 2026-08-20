a648c1c2063a5b365d9ef8f9d83f59fcb39b7133
Benedict Smith <benedict.smith@ukaea.uk>
2026-08-20 09:21:29 +0100

    Added parallel workflow

--- base_case/runMesh.sh
+++ base_case/runMesh.sh
@@ -1,6 +1,8 @@
 set -eou pipefail
-rm -rf constant/polyMesh constant/*/polyMesh dynamicCode
+rm -rf constant/polyMesh constant/*/polyMesh dynamicCode 0/cellToRegion processor* constant/cellToRegion
 blockMesh -dict system/blockMeshDict 2>&1 | tee log.blockMesh
-decomposePar 
-snappyHexMesh -overwrite 2>&1 | tee log.snappyHexMesh
+decomposePar 2>&1 | tee log.decomposePar
+mpirun -np 4 snappyHexMesh -parallel -overwrite 2>&1 | tee log.snappyHexMesh
+reconstructParMesh -constant 2>&1 | tee log.reconstructParMesh
+reconstructPar -constant 2>&1 | tee log.reconstructPar
 splitMeshRegions -cellZones -overwrite 2>&1 | tee log.splitMeshRegions
